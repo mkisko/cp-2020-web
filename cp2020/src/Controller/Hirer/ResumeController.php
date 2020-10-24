c1e310d5da3e919a145f5d1888cacf287b1ae66c
k.kaplin <k.kaplin@profsoft.pro>
2020-10-25 03:57:55 +0400

    added resume controller and route

---
+++ cp2020/src/Controller/Hirer/ResumeController.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Controller\Hirer;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * Class ResumeController
+ * @package App\Controller\Hirer
+ * @Route("/hirer/resume")
+ */
+class ResumeController extends AbstractController {
+
+    /**
+     * @Route("/", name="resume_list", methods={"GET", "POST"})
+     * @return Response
+     */
+    public function index() {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        return $this->render('hirer/resume/index.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="resume_show", methods={"GET", "POST"})
+     * @param $id
+     * @return Response
+     */
+    public function show($id) {
+
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+
+        return $this->render('hirer/resume/show.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+}
